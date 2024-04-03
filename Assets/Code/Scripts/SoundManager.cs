38a6e89515cac2f9f6c40ee116c255192a0703e7
Jason Perez <jperez00100100@gmail.com>
2024-04-02 21:35:12 -0400

    Created SoundManager object and script

---
+++ Assets/Code/Scripts/SoundManager.cs
@@ -0,0 +1,14 @@
+// ---------------------------------------
+// Creation Date: 04/02/24
+// Author: Jason Perez
+// Modified By: Jason Perez
+// ---------------------------------------
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    
+}
