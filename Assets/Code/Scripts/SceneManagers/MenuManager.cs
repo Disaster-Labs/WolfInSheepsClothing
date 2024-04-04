8af2a1d4ac9186cbeff50cc1c725cf880dadee30
Boyi Qian <boyiq@andrew.cmu.edu>
2024-04-04 01:41:41 -0400

    complete credits, main menu, pause scene

--- Assets/Code/Scripts/SceneManagers/MenuManager.cs
+++ Assets/Code/Scripts/SceneManagers/MenuManager.cs
@@ -1,14 +1,48 @@
 // ---------------------------------------
-// Creation Date:
-// Author: 
+// Creation Date: 04/04/2024
+// Author: Boyi Qian
 // Modified By:
 // ---------------------------------------
 
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MenuManager : MonoBehaviour
 {
+    // public GameObject HowToCanvas;
+        
+    public void Btn_QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void Btn_StartGame()
+    {
+        SceneManager.LoadScene(GameManager.GAME_SCENE);
+        Time.timeScale = 1f;
+    }
+    public void Btn_Credits()
+    {
+        
+        SceneManager.LoadScene(GameManager.CREDITS_SCENE);
+    }
+    public void Btn_BackMenu()
+    {
+        
+        SceneManager.LoadScene(GameManager.MAIN_MENU_SCENE);
+        Time.timeScale = 1f;
+    }
+
+    // public void Btn_HowTo(){
+    //     HowToCanvas.SetActive(true);
+    // }
+
+    // public void Btn_CloseHowTo(){
+    //     HowToCanvas.SetActive(false);
+    // }
+
+
     
 }
