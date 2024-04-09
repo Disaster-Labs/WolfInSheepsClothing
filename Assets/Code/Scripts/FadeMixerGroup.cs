52ef1f463cf23bb8eed7d21a3de60b240b0f0672
Jason Perez <jperez00100100@gmail.com>
2024-04-09 16:57:42 -0400

    Created Audio Mixer and FadeMixerGroup script

---
+++ Assets/Code/Scripts/FadeMixerGroup.cs
@@ -0,0 +1,14 @@
+// ---------------------------------------
+// Creation Date:
+// Author: 
+// Modified By:
+// ---------------------------------------
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FadeMixerGroup : MonoBehaviour
+{
+    
+}
