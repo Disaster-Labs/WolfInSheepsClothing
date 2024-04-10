d7c058139ba26e1608cd18c9ff7c033cfe1eed52
Jason Perez <jperez00100100@gmail.com>
2024-04-10 16:59:22 -0400

    Updated SoundManager to handle audio when Wolf is moving and imported wolf walking sound effect

--- Assets/Code/Scripts/SoundManager.cs
+++ Assets/Code/Scripts/SoundManager.cs
@@ -7,27 +7,49 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.Audio;
 using UnityEngine;
 
 public class SoundManager : MonoBehaviour
 {
     // Events
-    [SerializeField] private WolfMovement wolf;
+    //[SerializeField] private WolfMovement wolf;
+    public event EventHandler<Boolean> WolfWalking;
 
     // Audio Sources
     private AudioSource audioSrc;
     [SerializeField] private AudioSource backgroundSrc;
+    [SerializeField] private AudioSource wolfAudio;
+
+    // Audio Clips
+    [SerializeField] private AudioClip wolfWalkingAudio;
 
     void Awake() {
-        wolf.PlayBGMusic += PlayBGMusic;
+        WolfWalking += PlayWalkingAudio;
     }
 
     void Start() {
         audioSrc = GetComponent<AudioSource>();
     }
 
-    public void PlayBGMusic(object sender, EventArgs e) {
-        backgroundSrc.Play();
-        Debug.Log("Play BG Music");
+    void Update() {
+        if (Input.GetKeyDown("w")) {
+            Debug.Log("Wolf started walking");
+            WolfWalking.Invoke(this, true);
+        }
+
+        if (Input.GetKeyUp("w")) {
+            Debug.Log("Wolf stopped walking");
+            WolfWalking.Invoke(this, false);
+        }
+    }
+
+    public void PlayWalkingAudio(object sender, Boolean isWalking ) {
+        if (isWalking) {
+            wolfAudio.clip = wolfWalkingAudio;
+            wolfAudio.Play();
+        } else {
+            wolfAudio.Stop();
+        }
     }
 }
