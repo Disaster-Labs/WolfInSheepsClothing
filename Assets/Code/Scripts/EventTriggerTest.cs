cef1e9292237405a5caa7b805b9324e32072e1f7
Boyi Qian <boyiq@andrew.cmu.edu>
2024-04-24 03:12:57 -0400

    update scoreboard

--- Assets/Code/Scripts/EventTriggerTest.cs
+++ Assets/Code/Scripts/EventTriggerTest.cs
@@ -15,13 +15,13 @@ public class EventTriggerTest : MonoBehaviour
 
         if (Input.GetKeyDown(KeyCode.F))
         {
-
+            Debug.Log("Dummy eat trigger!");
             GameManager.TriggerSheepEaten();
         }
 
         if (Input.GetKeyDown(KeyCode.G))
         {
-
+            Debug.Log("Dummy hit trigger!");
             GameManager.TriggerWolfHit();
         }
     }
